e5a8825b83ed7e30c1bf4fd22cec5517cd162363
edusfreitas23 <eduardosantos.f23@gmail.com>
2024-03-19 00:55:30 -0300

    DESAFIO DIO

---
+++ Conta/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // TODO: Conhecer e importar a classe Scanner;
+        // Exibir as mensagens para o nosso usuario;
+        // Obter pela Scanner
+
+    }
+}
